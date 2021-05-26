394dbf2c7d1a634990f8b3ced33cda5a2c34a3a0
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-05-26 14:11:00 -0300

    Navegation with pages/ and databinding with components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] =  [
 
-    new Recipe('Test Recipe','Description here','https://cdn.pixabay.com/photo/2020/05/11/21/57/bake-5160388_1280.jpg')
+    new Recipe('Test Recipe','Description here','https://cdn.pixabay.com/photo/2020/05/11/21/57/bake-5160388_1280.jpg'),
+
+    new Recipe('Recipe 2','Other description here','https://cdn.pixabay.com/photo/2020/05/11/21/57/bake-5160388_1280.jpg')
 
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
