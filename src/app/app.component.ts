83832f87a0338c1350287f307b7a97dec091240a
Gabriielfeliciano <gabriel.feliciano123@gmail.com>
2021-06-09 09:48:52 -0300

    Fix bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-<<<<<<< HEAD
   // numbers = [1, 2, 3, 4, 5];
   oddNumbers = [1 , 3, 5];
   evenNumbers = [2, 4];
   onlyOdd = false;
   value = 5;
-=======
 
->>>>>>> a27c5857a0ecdf5dc64f2ae0d82ce1c45e4ad3db
 }
